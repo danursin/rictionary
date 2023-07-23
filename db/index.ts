6e11841cf97d2d92396f43c14aadc30b12e8ce8d
danursin <danursin@gmail.com>
2023-07-23 10:42:56 -0500

    fix: specify region

--- db/index.ts
+++ db/index.ts
@@ -3,6 +3,7 @@ import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 const client = new DynamoDBClient({
+    region: "us-east-1",
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
